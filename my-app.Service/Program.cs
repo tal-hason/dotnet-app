91660f6a58716a627613500ab62f7ad56e37f44d
tal-hason <thason@redhat.com>
2024-05-06 15:42:35 +0300

    fix stuffffff

--- my-app.Service/Program.cs
+++ my-app.Service/Program.cs
@@ -19,11 +19,9 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
-}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 
 app.UseHttpsRedirection();
 
