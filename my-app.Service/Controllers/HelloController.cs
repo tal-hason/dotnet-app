b3ae29416ec4b967ce2aa7561e10e9203e9e0e5f
tal-hason <thason@redhat.com>
2024-05-06 14:29:45 +0300

    first commit

---
+++ my-app.Service/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_app.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("hello")]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
---
+++ my-app.Service/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Check for the presence of the PORT environment variable
+var port = Environment.GetEnvironmentVariable("PORT");
+if (string.IsNullOrEmpty(port))
+{
+    port = "8080"; // Default port if PORT environment variable is not set
+}
+
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () => "Hello, World!");
+
+app.MapControllers();
+
+app.Run();
