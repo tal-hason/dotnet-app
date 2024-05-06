bfa3a2094ec60e698c3094bb62d3a32eac6c3b8a
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2024-05-06 21:12:09 +0000

    created developer template

--- init_env.sh
+++ init_env.sh
@@ -13,23 +13,33 @@ read -p "Enter your email address: " EMAIL
 yq e ".metadata.generateName = \"$WORKSHOP_USER-dotnet-app-\"" -i PipelineRun.yaml
 yq e ".spec.params[0].value = \"$GIT_REPO_URL\"" -i PipelineRun.yaml
 yq e ".spec.params[1].value = \"$GITHUB_USERNAME\"" -i PipelineRun.yaml
-yq e ".spec.params[1].value = \"$GITHUB_USERNAME\"" -i PipelineRun.yaml
+yq e ".spec.params[4].value = \"$WORKSHOP_USER-application\"" -i PipelineRun.yaml
+yq e ".spec.pipelineRef.name = \"$WORKSHOP_USER-dotnet-app\"" -i PipelineRun.yaml
 
 # Get the latest OC client
-wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz -o /tmp
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+wget https://mirror.openshift.com/pub/openshift-v4/clients/pipeline/latest/tkn-linux-amd64.tar.gz
 
 tar xvf openshift-client-linux.tar.gz --no-same-owner
+tar xvf tkn-linux-amd64.tar.gz --no-same-owner
+
 
 rm -rf /tmp/runs
 mkdir /tmp/runs
+chmod +x yq_linux_amd64 
+mv yq_linux_amd64 /tmp/runs/yq
+mv oc kubectl tkn tkn-pac opc /tmp/runs
 
-mv oc kubectl /tmp/runs
+export PATH=$(echo "${PATH}:/tmp/runs")
 
-export PATH=$(echo ${PATH}:/tmp/runs)
+# Set the WORKSHOP_USER environment variable
+export WORKSHOP_USER
 
-rm openshift-client-linux.tar.gz README.md
+rm openshift-client-linux.tar.gz README.md tkn-linux-amd64.tar.gz LICENSE
 
-oc login -u $WORKSHOP_USER -p $WORKSHOP_PASSWORD $WORKSHOP_API_URL
+oc login -u $WORKSHOP_USER -p $WORKSHOP_PASSWORD $WORKSHOP_API_URL --insecure-skip-tls-verify=true
+
 
 # Create the k8s imagePull Secret for ghcr.io
 oc create secret docker-registry ghcr-secret \
---
+++ start_pipeline.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Extract the numeric part of the tag
+TAG=$(yq eval ".spec.params[3].value" PipelineRun.yaml)
+NUMERIC_TAG=$(echo $TAG | sed 's/[^0-9]*//g')
+
+# Increment the numeric part of the tag by one
+NEW_NUMERIC_TAG=$((NUMERIC_TAG + 1))
+
+# Construct the new tag with the incremented numeric part
+NEW_TAG="v$NEW_NUMERIC_TAG"
+
+# Update the PipelineRun.yaml file with the new tag value
+yq eval ".spec.params[3].value = \"$NEW_TAG\"" -i PipelineRun.yaml
+
+echo "Increased image version tag to $NEW_TAG"
+
+# Run the PipelineRun and capture the output
+PR_OUTPUT=$(oc create -f PipelineRun.yaml -n $WORKSHOP_USER-argocd)
+
+# Extract the PipelineRun name from the output
+PIPELINE_RUN_NAME=$(echo "$PR_OUTPUT" | awk '{print $1}' | awk -F/ '{print $NF}')
+
+echo "Running New Pipeline with PipelineRun: $PIPELINE_RUN_NAME"
+
+# Get the logs for the PipelineRun
+tkn pr logs $PIPELINE_RUN_NAME -n $WORKSHOP_USER-argocd -f
+
+export APP_LINK=https://$(oc get route dotnet-app -n $WORKSHOP_USER-application -o yaml | yq '.spec.host')/swagger/index.html
+
+echo "To access the Application use this  link: ${APP_LINK}"
