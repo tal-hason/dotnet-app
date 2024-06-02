defe378863c4eb3124703ca13a07d09d3fa2dae0
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2024-06-02 14:01:56 +0000

    new image tag

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -5,7 +5,7 @@ export PATH=$(echo "${PATH}:/tmp/runs")
 source env_vars.sh
 
 # Extract the numeric part of the tag
-TAG=$(yq eval ".spec.params[3].value" PipelineRun.yaml)
+TAG=$(yq eval ".spec.params[3].value" GitOps/PipelineRun.yaml)
 NUMERIC_TAG=$(echo $TAG | sed 's/[^0-9]*//g')
 
 # Increment the numeric part of the tag by one
@@ -14,8 +14,8 @@ NEW_NUMERIC_TAG=$((NUMERIC_TAG + 1))
 # Construct the new tag with the incremented numeric part
 NEW_TAG="v$NEW_NUMERIC_TAG"
 
-# Update the PipelineRun.yaml file with the new tag value
-yq eval ".spec.params[3].value = \"$NEW_TAG\"" -i PipelineRun.yaml
+# Update the GitOps/PipelineRun.yaml file with the new tag value
+yq eval ".spec.params[3].value = \"$NEW_TAG\"" -i GitOps/PipelineRun.yaml
 
 echo "Increased image version tag to $NEW_TAG"
 
@@ -30,6 +30,14 @@ echo "Running New Pipeline with PipelineRun: $PIPELINE_RUN_NAME"
 # Get the logs for the PipelineRun
 tkn pr logs $PIPELINE_RUN_NAME -n $WORKSHOP_USER-argocd -f
 
-export APP_LINK=https://$(oc get route $WORKSHOP_USER-dotnet-app -n $WORKSHOP_USER-application -o yaml | yq '.spec.host')/swagger/index.html
+# Patch the Values file with the new image and tag
+yq eval ".image.name = \"ghcr.io\$GITHUB_USERNAME\"" -i GitOps/values.yaml
+yq eval ".image.tag = \"$NEW_TAG\"" -i GitOps/values.yaml
+
+git commit -am "new image tag"
+
+git push
+
+export APP_LINK=https://$(oc get ingress $WORKSHOP_USER-dotnet-app -n $WORKSHOP_USER-application -o yaml | yq '.spec.rules[0].host')/swagger/index.html
 
 echo "To access the Application use this  link: ${APP_LINK}"
