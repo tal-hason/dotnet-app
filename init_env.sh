f3afa4becb797e9c79ef4b9a4a1a8421f8c9e25b
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2024-06-02 11:52:59 +0000

    Init Env ended

--- init_env.sh
+++ init_env.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Read input from the user
-read -p "Enter the Git-Hub repository URL: " GIT_REPO_URL
-read -p "Enter the workshop user: " WORKSHOP_USER
-read -p "Enter the workshop Cluster API URL: " WORKSHOP_API_URL
-read -p "Enter the workshop Password: " WORKSHOP_PASSWORD
 read -p "Enter the Git-Hub username: " GITHUB_USERNAME
 read -p "Enter your GitHub personal access token: " GHCR_TOKEN
 read -p "Enter your email address: " EMAIL
+read -p "Enter the Git-Hub repository URL: " GIT_REPO_URL
+read -p "Enter the workshop cluster FQDN: " CLUSTER_FQDN
+read -p "Enter the workshop user: " WORKSHOP_USER
+read -p "Enter the workshop Cluster API URL: " WORKSHOP_API_URL
+read -p "Enter the workshop Password: " WORKSHOP_PASSWORD
 
-# Update the PipelineRun.yaml file
-yq e ".metadata.generateName = \"$WORKSHOP_USER-dotnet-app-\"" -i PipelineRun.yaml
-yq e ".spec.params[0].value = \"$GIT_REPO_URL\"" -i PipelineRun.yaml
-yq e ".spec.params[1].value = \"$GITHUB_USERNAME\"" -i PipelineRun.yaml
-yq e ".spec.params[4].value = \"$WORKSHOP_USER-application\"" -i PipelineRun.yaml
-yq e ".spec.pipelineRef.name = \"$WORKSHOP_USER-dotnet-app\"" -i PipelineRun.yaml
+
 
 # Get the latest OC client
 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
@@ -38,18 +34,40 @@ export WORKSHOP_USER
 
 rm openshift-client-linux.tar.gz README.md tkn-linux-amd64.tar.gz LICENSE
 
+# Update the PipelineRun.yaml file
+yq e ".metadata.generateName = \"$WORKSHOP_USER-dotnet-app-\"" -i PipelineRun.yaml
+yq e ".spec.params[0].value = \"$GIT_REPO_URL\"" -i PipelineRun.yaml
+yq e ".spec.params[1].value = \"$GITHUB_USERNAME\"" -i PipelineRun.yaml
+yq e ".spec.params[4].value = \"$WORKSHOP_USER-application\"" -i PipelineRun.yaml
+yq e ".spec.pipelineRef.name = \"$WORKSHOP_USER-dotnet-app\"" -i PipelineRun.yaml
+
+# Update the argo-app.yaml file
+yq e ".spec.destination.namespace = \"$WORKSHOP_USER-application"\" -i GitOps/Argo-App.yaml
+yq e ".spec.sources[1].repoURL = \"$GIT_REPO_URL"\" -i GitOps/Argo-App.yaml
+
+# Update the values.yaml file
+yq e ".global.nameOverride = \"$WORKSHOP_USER"\" -i GitOps/values.yaml
+yq e ".global.namespace = \"$WORKSHOP_USER-application"\" -i GitOps/values.yaml
+yq e ".global.ClusterFqdn = \"$CLUSTER_FQDN"\" -i GitOps/values.yaml
+
 oc login -u $WORKSHOP_USER -p $WORKSHOP_PASSWORD $WORKSHOP_API_URL --insecure-skip-tls-verify=true
 
+oc project $WORKSHOP_USER-application
 
+oc apply -f GitOps/Argo-App.yaml -n $WORKSHOP_USER-argocd
 # Create the k8s imagePull Secret for ghcr.io
 oc create secret docker-registry ghcr-secret \
     --docker-server=ghcr.io \
     --docker-username=$GITHUB_USERNAME \
     --docker-password=$GHCR_TOKEN \
-    --docker-email=$EMAIL
+    --docker-email=$EMAIL \
+    -n $WORKSHOP_USER-argocd
 
 # Patch the serviceAccount pipeline to use the imagePull Secret
 oc patch serviceaccount pipeline -p '{"imagePullSecrets":[{"name":"ghcr-secret"}]}'
 
 # Add the created secret as a secret to the service account
 oc patch serviceaccount pipeline -p '{"secrets":[{"name":"ghcr-secret"}]}'
+
+# Commit latest Changes
+git commit -am "Init Env ended"
