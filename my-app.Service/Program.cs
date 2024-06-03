811acc16cf3f0c664195071c4418e347a66fb30d
sorelsh <67732616+sorelsh@users.noreply.github.com>
2024-06-03 12:19:10 +0000

    new image tag

--- my-app.Service/Program.cs
+++ my-app.Service/Program.cs
@@ -25,7 +25,7 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1")
 
 app.UseHttpsRedirection();
 
-app.MapGet("/hello", () => "Hello, World!");
+app.MapGet("/hello", () => "Hello, RedHat!");
 
 app.MapControllers();
 
